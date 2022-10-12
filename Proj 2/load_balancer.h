4a1b0b91d80b80924f83821c4aacb7d8c384d622
nadxhielijuarez <nadxhielijuarez@gmail.com>
2022-10-12 00:46:00 -0500

    finally got doxygen working

--- Proj 2/load_balancer.h
+++ Proj 2/load_balancer.h
@@ -2,5 +2,10 @@
 #ifndef MY_CLASS_H // include guard
 #define MY_CLASS_H
 
+/*
+where the queue is used. 
+
+*/
+
 
 #endif /* MY_CLASS_H */
--- Proj 2/main.cpp
+++ Proj 2/main.cpp
@@ -1,1 +1,10 @@
 #include <iostream>
+
+
+/*
+queue gets instantiated with server*2 random requests
+
+the load balancer comes into to play to select which request to put into the web_server
+web server will hold request for the time alloted then it'll print the processed request to the one its taking in
+
+*/
