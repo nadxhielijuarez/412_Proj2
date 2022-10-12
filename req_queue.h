927a9838df2d01591b07f380c648188a022275aa
nadxhielijuarez <nadxhielijuarez@gmail.com>
2022-10-12 12:56:25 -0500

    might be done with web_server
    Might have finished web_server

--- req_queue.h
+++ req_queue.h
@@ -7,15 +7,26 @@
 
 using namespace std;
 class Req_Queue{
-    Req_Queue();
+    Req_Queue(int servers);
+    int max_time;
+    int size;
+    queue<Request> requests;
 };
 
 /**
  * @brief Might not need it
  * 
  */
-Req_Queue::Req_Queue(){
-    /*why is the class not being recognized?*/
-   //queue<request> g;
+Req_Queue::Req_Queue(int servers){
+    int find_max_time = -1;
+    size = 2*servers;
+    for(int i=0; i<size; i++){
+        Request newRequest;
+        requests.push(newRequest);
+        if(find_max_time< newRequest.time){
+            find_max_time = newRequest.time;
+        }
+    }
+    max_time = find_max_time;
 }
 #endif 
--- web_server.h
+++ web_server.h
@@ -11,21 +11,59 @@ request inside server just deletes when time is over
 */
 
 /**
- * @brief A dynamic array
+ * @brief THis is essentially a dynamic array
  * 
  */
 class Web_Server{
     int server_size;
     Request* server;/*should point to the array of that size*/
     Web_Server(int size);
-    void fill_next_available_slot();
+    ~Web_Server();
+    int process_requests(Request newRequest,  int max_req_time, int starting_index);
 
 };
-
+/**
+ * @brief Construct a new Web_Server::Web_Server object, this instantiates my array to be of the size that is passed
+ * 
+ * @param size 
+ */
 Web_Server::Web_Server(int size){
     server_size = size;
     server = new Request[size];
 }
+/**
+ * @brief Destroy the Web_Server::Web_Server object Destructor
+ * 
+ */
+Web_Server::~Web_Server(){
+    delete [] server;
+}
+
+/**
+ * @brief Servers to process all 
+ * 
+ * @param newRequest 
+ * @param max_size 
+ * @param starting_index 
+ * @return int 
+ */
+int Web_Server::process_requests(Request newRequest, int max_size, int starting_index){
+    int processed_index = 0;
+    for(int i=starting_index; i<max_size; i++){
+        for(int j=0; j<server_size; j++){
+            Request cur_request = server[j];
+            if(cur_request.time == i){
+                processed_index = i;
+                cout<<"Processed request: "<<cur_request.output_IP<<" begining "<<newRequest.input_IP <<endl;
+                server[j] = newRequest; 
+                break;
+            }
+        }
+    }
+    return processed_index;
+}
+
+
 
 
 #endif /* MY_CLASS_H */
