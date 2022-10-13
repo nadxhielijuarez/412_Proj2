96e5e9521044126543a06acccc44b6d45f14593c
nadxhielijuarez <nadxhielijuarez@gmail.com>
2022-10-13 17:33:11 -0500

    read in parameters from commandline

--- main.cpp
+++ main.cpp
@@ -1,55 +1,45 @@
 #include <iostream>
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 #include "web_server.h"
 #include "request.h"
 #include "load_balancer.h"
 #include "req_queue.h"
 
 using namespace std;
-int main(){
+int main(int argc, char *argv[]){
     /*
     User input:
     - time to run load balancer, in microseconds, max is 100000
-    - number of servers
-    */
-   cout<<"Enter the number of servers you would like"<<endl;
-   string str_servers; 
-   cin>>str_servers;
-   int servers = stoi(str_servers);
-   cout<<"read in: "<<str_servers<<" servers" <<endl;
 
-   cout<<"Enter the time for which you would like to run the load_balancer in microseconds, max is 100000"<<endl;
-   string str_lb_time;
-   cin>> str_lb_time;
-   int lb_time = stoi(str_lb_time);
-   cout<<"read in: "<<str_lb_time<<" load balancer time " <<endl;
+    queue gets instantiated with server*2 random requests
+
+    the load balancer comes into to play to select which request to put into the web_server
+    web server will hold request for the time alloted then it'll print the processed request to the one its taking in
+
+    number of servers
+    */
+   int servers;
+   int lb_time;
+   int c;
+    while ((c = getopt (argc, argv, "s:t:")) != -1){
+            switch (c){
+                case 's':
+                    servers = atoi (optarg);
+                    break;
+                case 't':
+                    lb_time = atoi (optarg);
+                // cout<<"read in m?"<<endl;
+                    break;
+    }
 
    Req_Queue myQ(servers);
-
-//    cout<<"queue made: "<<endl;
-//    for(int i=0; i< servers;i++){
-//     Request* req = myQ.requests.front();
-//     cout<<"req: "<<req->input_IP<<endl;
-//     myQ.pop();
-//    }
    Web_Server myServer(servers);
-  Load_Balancer myLB(myQ, myServer, lb_time);
+   Load_Balancer myLB(myQ, myServer, lb_time);
 
-//    cout<<"checking server:"<<endl;
-
-//    for(int i = 0; i<servers; i++){
-//     cout<<"renamed?"<<myServer.server[i].name<<endl;
-//    }
-
-
-
-   cout<<"\n Beginning to allocate requests"<<endl;
-   myLB.allocate_requests();
-
+   cout<<"\n Beginning to allocate requests, requests time vary from 0-1000"<<endl;
+   myLB.allocate_requests();  
+    }
 }
-/*
-queue gets instantiated with server*2 random requests
-
-the load balancer comes into to play to select which request to put into the web_server
-web server will hold request for the time alloted then it'll print the processed request to the one its taking in
-
-*/
